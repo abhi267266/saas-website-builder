2c6949a0198118a07f7408add27d10f06d4e493d
Abhishek pathak <abhi267266@gmail.com>
2024-03-13 21:39:14 +0530

    added icons configuration in the constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,31 @@
+import { ClipboardIcon } from "lucide-react"
+import Payment from "../../icons/payment"
+import Person from "../../icons/person"
+import PluraCategory from "../../icons/plura-category"
+import Power from "../../icons/power"
+import Receipt from "../../icons/receipt"
+import Shield from "../../icons/shield"
+import Star from "../../icons/star"
+import Tune from "../../icons/tune"
+import Video from "../../icons/video_recorder"
+import Wallet from "../../icons/wallet"
+import Warning from "../../icons/warning"
+import BarChart from "../../icons/bar_chart"
+import Headphone from "../../icons/headphone"
+import Send from "../../icons/send"
+import Pipelines from "../../icons/pipelines"
+import Calendar from "../../icons/calendar"
+import Settings from "../../icons/settings"
+import CheckCircle from "../../icons/check_circled"
+import Chip from "../../icons/chip"
+import Compass from "../../icons/compass"
+import Database from "../../icons/database"
+import Flag from "../../icons/flag"
+import Home from "../../icons/home"
+import Info from "../../icons/info"
+import LinkIcon from "../../icons/link"
+import Message from "../../icons/messages"
+
 export const pricingCards = [
     {
       title: 'Starter',
@@ -25,5 +53,153 @@ export const pricingCards = [
       highlight: 'Everything in Starter, plus',
       features: ['Unlimited Sub accounts', 'Unlimited Team members'],
       priceId: 'price_1OYxkqFj9oKEERu1NbKUxXxN',
+    },
+  ]
+
+  export const icons = [
+    {
+      value: 'chart',
+      label: 'Bar Chart',
+      path: BarChart,
+    },
+    {
+      value: 'headphone',
+      label: 'Headphones',
+      path: Headphone,
+    },
+    {
+      value: 'send',
+      label: 'Send',
+      path: Send,
+    },
+    {
+      value: 'pipelines',
+      label: 'Pipelines',
+      path: Pipelines,
+    },
+    {
+      value: 'calendar',
+      label: 'Calendar',
+      path: Calendar,
+    },
+    {
+      value: 'settings',
+      label: 'Settings',
+      path: Settings,
+    },
+    {
+      value: 'check',
+      label: 'Check Circled',
+      path: CheckCircle,
+    },
+    {
+      value: 'chip',
+      label: 'Chip',
+      path: Chip,
+    },
+    {
+      value: 'compass',
+      label: 'Compass',
+      path: Compass,
+    },
+    {
+      value: 'database',
+      label: 'Database',
+      path: Database,
+    },
+    {
+      value: 'flag',
+      label: 'Flag',
+      path: Flag,
+    },
+    {
+      value: 'home',
+      label: 'Home',
+      path: Home,
+    },
+    {
+      value: 'info',
+      label: 'Info',
+      path: Info,
+    },
+    {
+      value: 'link',
+      label: 'Link',
+      path: LinkIcon,
+    },
+    {
+      value: 'lock',
+      label: 'Lock',
+      path: Lock,
+    },
+    {
+      value: 'messages',
+      label: 'Messages',
+      path: Message,
+    },
+    {
+      value: 'notification',
+      label: 'Notification',
+      path: Notification,
+    },
+    {
+      value: 'payment',
+      label: 'Payment',
+      path: Payment,
+    },
+    {
+      value: 'power',
+      label: 'Power',
+      path: Power,
+    },
+    {
+      value: 'receipt',
+      label: 'Receipt',
+      path: Receipt,
+    },
+    {
+      value: 'shield',
+      label: 'Shield',
+      path: Shield,
+    },
+    {
+      value: 'star',
+      label: 'Star',
+      path: Star,
+    },
+    {
+      value: 'tune',
+      label: 'Tune',
+      path: Tune,
+    },
+    {
+      value: 'videorecorder',
+      label: 'Video Recorder',
+      path: Video,
+    },
+    {
+      value: 'wallet',
+      label: 'Wallet',
+      path: Wallet,
+    },
+    {
+      value: 'warning',
+      label: 'Warning',
+      path: Warning,
+    },
+    {
+      value: 'person',
+      label: 'Person',
+      path: Person,
+    },
+    {
+      value: 'category',
+      label: 'Category',
+      path: PluraCategory,
+    },
+    {
+      value: 'clipboardIcon',
+      label: 'Clipboard Icon',
+      path: ClipboardIcon,
     },
   ]
